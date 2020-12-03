dc89c1fc9c44d958336b506f23727a8c5ab863a1
minacokamura <74166958+minacokamura@users.noreply.github.com>
2020-12-02 19:41:12 -0800

    Added JavaScript for the hamburger menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Welcome to my portfolio website. Thank you for stopping by!");
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
