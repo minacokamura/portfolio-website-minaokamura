736f42e9d5d8260d268334143975164db7437388
minacokamura <74166958+minacokamura@users.noreply.github.com>
2020-12-08 12:08:05 -0800

    Edited JavaScript to fix the errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-console.log("Welcome to my portfolio website. Thank you for stopping by!");
+console.log('Welcome to my portfolio website. Thank you for stopping by!')
 
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
